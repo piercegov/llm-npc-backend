6233d33146817cfb35cbc0f57eedc387c1a03db2
Pierce Governale <piercegovernale@gmail.com>
2025-05-28 19:32:18 -0700

    Add /health endpoint

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -16,6 +16,11 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "LLM NPC Backend is running!")
 	})
-
+	
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "pong")
+	})
+	
 	log.Fatal(http.ListenAndServe(":"+config.Port, nil))
 }
